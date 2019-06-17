b93a1e308e78f5b82751fc1ec7c1ee90436b194e
14Spaseeva <stasiaspaseeva@gmail.com>
2019-06-17 04:19:20 +0300

    КОНЕЦ КОНЕЦ КОНЕЦ АААААААААААААААА

---
+++ res/CommandLib.java
@@ -0,0 +1,33 @@
+package main.examples;
+
+import main.bdd.CommandLibable;
+import main.bdd.annotation.Recieve;
+import main.bdd.annotation.Send;
+
+public class CommandLib extends CommandLibable {
+
+    @Send(text = "Ignition On")
+    public void ignitionOn() {
+        xlOpenDriver();
+        xlOpenPort();
+    }
+
+    @Send(text = "Ignition Off")
+    public void ignitionOff() {
+        xlDeactivateChannel();
+        xlClosePort();
+        xlCloseDriver();
+    }
+
+
+    @Send(text = "request [param1]")
+    public void sendRequest(String param1) {
+        xlFrTransmit(HexUtil.fromStringtoHexArray(param1));
+    }
+
+    @Recieve(text = "response [param1]")
+    public void recieveResponse(String param1) {
+        xlFrReceive(HexUtil.fromStringtoHexArray(param1));
+    }
+
+}
